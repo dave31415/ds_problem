4db73b87ea3a40884e7a20e4f2921b995a7f3516
David Johnston <dajohnst@thoughtworks.com>
2020-10-28 14:45:34 -0400

    removed memo

--- memo.py
+++
@@ -1,7 +0,0 @@
-def memo(f):
-    """ Memoization decorator for a function taking a single argument """
-    class memodict(dict):
-        def __missing__(self, key):
-            ret = self[key] = f(key)
-            return ret
-    return memodict().__getitem__
