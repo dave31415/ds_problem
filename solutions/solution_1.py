5f732b7bfba2247fbd6cc002d3ef6f007bc47238
David Johnston <dajohnst@thoughtworks.com>
2021-01-21 22:04:54 -0500

    refactor

--- solutions/solution_1.py
+++ solutions/solution_1.py
@@ -13,14 +13,13 @@ def run_solution_1():
     for line in corrupted:
         values = [int(i) for i in list(line.values())]
         values_sorted = sorted(values)
-
         cust_id = values_sorted[2]
         age = values_sorted[0]
         amount = values_sorted[1]
 
         if age < 18:
-            age = amount
-            amount = values_sorted[0]
+            # swap age and amount, helps very little
+            age, amount = amount, age
 
         values_fixed = [cust_id, age, amount]
         row_fixed = {k: v for k, v in zip(field_names, values_fixed)}
